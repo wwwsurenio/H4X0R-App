e07d80d95c139ed7f7865cde30a1046326124da8
Suren Oganesian <me@suren.io>
2023-01-29 10:26:56 +0100

    Initial Commit

---
+++ H4X0R News/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by User01 on 29/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
