51903027ed0bfcacb0f32436e85164ebe3ca6c35
Suren Oganesian <me@suren.io>
2023-01-30 23:07:47 +0100

    App for reading news from the Hacker News website, final version

---
+++ H4X0R News/View/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by User01 on 29/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView{
+            List(networkManager.posts){ post in
+                NavigationLink(destination: DetailView(url: post.url)){
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationBarTitle("H40R News")
+        }
+        .onAppear {
+            networkManager.fetchData()
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+//let posts = [
+//    Post(id: "1", title: "Hello"),
+//    Post(id: "2", title: "Bonjour"),
+//    Post(id: "3", title: "Barev")
+//]
+
---
+++ H4X0R News/View/WebView.swift
@@ -0,0 +1,31 @@
+//
+//  WebView.swift
+//  H4X0R News
+//
+//  Created by User01 on 30/1/23.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+
+struct WebView: UIViewRepresentable{
+    
+    let urlString: String?
+
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString){
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+                
+        }
+    }
+    
+}
